86af234069d61aff340459d5cbc99f60ff82d19e
Antonin Carlin <antonin.carlin@icloud.com>
2021-08-06 16:10:03 +0200

    new test

--- dist/tests/test.js
+++ dist/tests/test.js
@@ -4,17 +4,21 @@ const index_1 = require("../index");
 const promises_1 = require("fs/promises");
 const path_1 = require("path");
 const svg2img = require("svg2img");
-(async () => {
+async function testOne(filename, density = [300, 400, 500]) {
+    const svg = await promises_1.readFile(path_1.resolve(__dirname, `./${filename}`), "utf8");
+    density.forEach((densite, index) => {
+        const pattern = index_1.default(svg, 1600, 1200, densite);
+        svg2img(pattern, { width: 1600, height: 1200 }, function (err, img) {
+            promises_1.writeFile(path_1.resolve(__dirname, `./generateSvgs/jpg/${filename.slice(0, filename.length - 4)}_densite-${index + 1}.jpg`), img);
+        });
+    });
+}
+async function testAll() {
     const svgFiles = (await promises_1.readdir(path_1.resolve(__dirname, "./"))).filter(file => file.endsWith(".svg"));
     console.log(svgFiles);
     svgFiles.forEach(async (file) => {
-        const svg = await promises_1.readFile(path_1.resolve(__dirname, `./${file}`), "utf8");
-        [300, 400, 500].forEach((densite, index) => {
-            const pattern = index_1.default(svg, 1600, 1200, densite);
-            svg2img(pattern, { width: 1600, height: 1200 }, function (err, img) {
-                promises_1.writeFile(path_1.resolve(__dirname, `./generateSvgs/jpg/${file.slice(0, file.length - 4)}_densite-${index + 1}.jpg`), img);
-            });
-        });
+        testOne(file);
     });
-})();
+}
+testOne("nageur_2.svg");
 //# sourceMappingURL=test.js.map
--- tests/test.ts
+++ tests/test.ts
@@ -3,7 +3,38 @@ import { writeFile, readFile, readdir } from "fs/promises";
 import { resolve } from "path";
 import { readFileSync, writeFileSync } from "fs";
 const svg2img = require("svg2img");
-(async () => {
+
+async function testOne(
+  filename: string,
+  density: number[] = [300, 400, 500]
+) {
+  const svg = await readFile(
+    resolve(__dirname, `./${filename}`),
+    "utf8"
+  );
+  density.forEach((densite, index) => {
+    const pattern = makePattern(svg, 1600, 1200, densite);
+
+    svg2img(
+      pattern,
+      { width: 1600, height: 1200 },
+      function (err: any, img: any) {
+        writeFile(
+          resolve(
+            __dirname,
+            `./generateSvgs/jpg/${filename.slice(
+              0,
+              filename.length - 4
+            )}_densite-${index + 1}.jpg`
+          ),
+          img
+        );
+      }
+    );
+  });
+}
+
+async function testAll() {
   // get all the .svg files in this directory
   const svgFiles = (
     await readdir(resolve(__dirname, "./"))
@@ -12,30 +43,8 @@ const svg2img = require("svg2img");
 
   // create a pattern from each file
   svgFiles.forEach(async file => {
-    const svg = await readFile(
-      resolve(__dirname, `./${file}`),
-      "utf8"
-    );
-
-    [300, 400, 500].forEach((densite, index) => {
-      const pattern = makePattern(svg, 1600, 1200, densite);
-
-      svg2img(
-        pattern,
-        { width: 1600, height: 1200 },
-        function (err: any, img: any) {
-          writeFile(
-            resolve(
-              __dirname,
-              `./generateSvgs/jpg/${file.slice(
-                0,
-                file.length - 4
-              )}_densite-${index + 1}.jpg`
-            ),
-            img
-          );
-        }
-      );
-    });
+    testOne(file);
   });
-})();
+}
+
+testOne("nageur_2.svg");
